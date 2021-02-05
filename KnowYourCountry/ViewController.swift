5c3bafe2d4cacfe334bf18ea93975b3b3fc236f3
nasfiu <reachnasfi@gmail.com>
2021-02-05 10:56:52 +0530

    initial commit

---
+++ KnowYourCountry/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KnowYourCountry
+//
+//  Created by Nasfi on 05/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
